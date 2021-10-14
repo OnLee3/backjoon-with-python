320986fb3f9209e0126c4c861cc51d31a33a3bd9
OnLee3 <dlwotjr2@gmail.com>
2021-10-14 11:45:19 +0900

    String

--- hello.py
+++ hello.py
@@ -1,15 +1,18 @@
 import sys
-
 N = int(sys.stdin.readline())
-cN = N
-flag = True
-answer = 0
+cnt = 0
 
-while flag == True:
-    ten = (cN % 10) * 10
-    one = (int(cN / 10) + cN % 10) % 10
-    cN = ten + one
-    answer += 1
-    if cN == N:
-        flag = False
-print(answer)
+for i in range(N):
+    S = sys.stdin.readline().rstrip()
+    flag = False
+    for j in range(len(S)):
+        linear = True
+        for k in range(j+1, len(S)):
+            if S[j] != S[k]:
+                linear = False
+            elif S[j] == S[k] and k - j > 1 and linear == False:
+                flag = True
+                break
+    if flag == False:
+        cnt += 1
+print(cnt)
