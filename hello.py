c88c7bd8c077770cf0d7b696c103e074622fcf26
OnLee3 <dlwotjr2@gmail.com>
2021-10-20 13:17:20 +0900

    설탕배달

--- hello.py
+++ hello.py
@@ -1,14 +1,13 @@
 import sys
-T = int(sys.stdin.readline())
-people = [[0 for j in range(14)] for i in range(15)]
-for i in range(15):
-    people[i][0] = 1
-for j in range(14):
-    people[0][j] = j + 1
-for i in range(1, 15):
-    for j in range(1, 14):
-        people[i][j] = people[i][j - 1] + people[i - 1][j]
-for _ in range(T):
-    k = int(sys.stdin.readline())
-    n = int(sys.stdin.readline())
-    print(people[k][n-1])
+N = int(sys.stdin.readline())
+cnt = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        cnt += (N // 5)
+        print(cnt)
+        break
+    N -= 3
+    cnt += 1
+else:
+    print(-1)
