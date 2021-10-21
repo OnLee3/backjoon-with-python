c5f355ad35ef9106ebb2aa18b1324789b44cd467
OnLee3 <dlwotjr2@gmail.com>
2021-10-22 08:05:21 +0900

    소수찾기

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
 import sys
 N = int(sys.stdin.readline())
-cnt = 0
+num_list = list(map(int, sys.stdin.readline().split()))
+answer = 0
 
-while N >= 0:
-    if N % 5 == 0:
-        cnt += (N // 5)
-        print(cnt)
-        break
-    N -= 3
-    cnt += 1
-else:
-    print(-1)
+for i in num_list:
+    if i == 1:
+        continue
+    cnt = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            cnt += 1
+    if cnt == 2:
+        answer += 1
+print(answer)
