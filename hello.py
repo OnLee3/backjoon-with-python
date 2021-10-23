bb507bdcd9960ea106edad3da13265c4bf49587c
OnLee3 <dlwotjr2@gmail.com>
2021-10-23 16:40:29 +0900

    소인수분해

--- hello.py
+++ hello.py
@@ -1,25 +1,11 @@
 import sys
 N = int(sys.stdin.readline())
-M = int(sys.stdin.readline())
-answer = []
 
-
-def solve(N, M):
-    for i in range(N, M+1):
-        if i == 1:
-            continue
-        flag = True
-        for j in range(2, i):
-            if i % j == 0:
-                flag = False
-                break
-        if flag == True:
-            answer.append(i)
-    if len(answer) > 0:
-        print(sum(answer))
-        print(min(answer))
-    else:
-        print(-1)
-
-
-solve(N, M)
+for i in range(2, N+1):
+    if i * i > N:
+        break
+    while N % i == 0:
+        print(i)
+        N = int(N / i)
+if N > 1:
+    print(N)
