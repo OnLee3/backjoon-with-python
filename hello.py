c83d2719d62a126d57d059a496134b5f19b14b56
OnLee3 <dlwotjr2@gmail.com>
2021-10-24 17:02:57 +0900

    소수구하기

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 import sys
-N = int(sys.stdin.readline())
+M, N = map(int, sys.stdin.readline().split())
+R = int(N ** 0.5)
+N += 1
+L = [True] * (N)
 
-for i in range(2, N+1):
-    if i * i > N:
-        break
-    while N % i == 0:
-        print(i)
-        N = int(N / i)
-if N > 1:
-    print(N)
+
+def solve(N, M):
+    for i in range(2, R+1):
+        if L[i] == True:
+            for j in range(i+i, N, i):
+                L[j] = False
+    for i in range(M, N):
+        if i > 1 and L[i] == True:
+            print(i)
+
+
+solve(N, M)
