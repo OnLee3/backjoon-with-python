480a4965e51e07c59259beb071f0d6aad3eb6ac2
OnLee3 <dlwotjr2@gmail.com>
2021-10-25 20:53:55 +0900

    골드바흐의 추측

--- hello.py
+++ hello.py
@@ -1,19 +1,27 @@
 import sys
 
 
-def solve(N):
-    R = int((N*2) ** 0.5)
-    M = ((N*2)+1)
-    L = [True] * M
-    for i in range(2, R+1):
+def solve(n):
+    for i in range(n//2, 1, -1):
         if L[i] == True:
-            for j in range(i+i, M, i):
-                L[j] = False
-    print(L[N+1:M].count(True))
+            for j in range(n//2, n):
+                if L[j] == True and i + j == n:
+                    print(i, j)
+                    break
+            else:
+                continue
+            break
 
 
-while True:
-    N = int(sys.stdin.readline())
-    if N == 0:
-        break
-    solve(N)
+R = int(10000 ** 0.5)
+L = [False, False, True] + [True, False] * 5000
+
+
+for i in range(2, R+1):
+    if L[i] == True:
+        for j in range(i+i, 10003, i):
+            L[j] = False
+
+for _ in range(int(sys.stdin.readline())):
+    n = int(sys.stdin.readline())
+    solve(n)
