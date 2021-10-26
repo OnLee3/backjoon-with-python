d5eb195837b7cb556d0dd5840e00cf706b69d144
OnLee3 <dlwotjr2@gmail.com>
2021-10-26 21:20:07 +0900

    네 번째점

--- hello.py
+++ hello.py
@@ -1,5 +1,21 @@
 import sys
-x, y, w, h = map(int, sys.stdin.readline().split())
-x = min(x, (w-x))
-y = min(y, (h-y))
-print(min(x, y))
+
+xList = []
+yList = []
+
+for i in range(3):
+    a, b = map(int, sys.stdin.readline().split())
+    xList.append(a)
+    yList.append(b)
+
+for i in xList:
+    if xList.count(i) == 1:
+        x = i
+        break
+
+for i in yList:
+    if yList.count(i) == 1:
+        y = i
+        break
+
+print(x, y)
