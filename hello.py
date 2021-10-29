9623412aa754c9cf70e9165d10365aef9139bea4
OnLee3 <dlwotjr2@gmail.com>
2021-10-29 17:40:02 +0900

    버블 정렬

--- hello.py
+++ hello.py
@@ -6,13 +6,10 @@ A = list(map(int, sys.stdin.readline().split()))
 
 def solve(A):
     for i in range(0, len(A)):
-        minIndex = i
-        for j in range(i+1, len(A)):
-            if A[j] < A[minIndex]:
-                minIndex = j
-        [A[i], A[minIndex]] = [A[minIndex], A[i]]
-    for i in A:
-        print(i)
+        for j in range(i+1, len(A)-i):
+            if A[j-1] > A[j]:
+                [A[j-1], A[j]] = [A[j], A[j-1]]
+    print(A)
 
 
 solve(A)
