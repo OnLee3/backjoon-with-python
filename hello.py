a7f8518af4811416898c4de301624c4ae3e5063d
OnLee3 <dlwotjr2@gmail.com>
2021-10-29 17:30:01 +0900

    선택정렬

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
 import sys
 import math
 
-R = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
 
-print(format(R**2*math.pi, ".6f"))
-print(format(R**2*2, ".6f"))
+
+def solve(A):
+    for i in range(0, len(A)):
+        minIndex = i
+        for j in range(i+1, len(A)):
+            if A[j] < A[minIndex]:
+                minIndex = j
+        [A[i], A[minIndex]] = [A[minIndex], A[i]]
+    for i in A:
+        print(i)
+
+
+solve(A)
