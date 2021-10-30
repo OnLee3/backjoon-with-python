bb4a22ab71536fe0f0b232254cebadbfd7e11785
LEON <dlwotjr2@gmail.com>
2021-10-31 07:59:24 +0900

    재귀함수

--- hello.py
+++ hello.py
@@ -1,20 +1,17 @@
 import sys
 
-T = int(sys.stdin.readline())
+N = int(sys.stdin.readline())
+
+cnt = 2
+answer = 1
 
 
-def solve(x1, y1, r1, x2, y2, r2):
-    r = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
-    if r == 0 and r1 == r2:
-        print(-1)
-    elif r1 + r2 == r or r2 == r + r1 or r1 == r + r2:
-        print(1)
-    elif max(r1, r2, r) > r1+r2+r-max(r1, r2, r):
-        print(0)
-    else:
-        print(2)
+def solve(N, cnt, answer):
+    if cnt > N:
+        return print(answer)
+    answer = answer * cnt
+    cnt += 1
+    solve(N, cnt, answer)
 
 
-for i in range(T):
-    x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
-    solve(x1, y1, r1, x2, y2, r2)
+solve(N, cnt, answer)
