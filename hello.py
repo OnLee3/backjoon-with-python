9a14d7e484307db54762a2306772b586fdc04465
LEON <dlwotjr2@gmail.com>
2021-10-30 20:46:02 +0900

    터렛

--- hello.py
+++ hello.py
@@ -1,15 +1,20 @@
 import sys
-import math
 
-A = list(map(int, sys.stdin.readline().split()))
-
-
-def solve(A):
-    for i in range(0, len(A)):
-        for j in range(i+1, len(A)-i):
-            if A[j-1] > A[j]:
-                [A[j-1], A[j]] = [A[j], A[j-1]]
-    print(A)
+T = int(sys.stdin.readline())
 
 
-solve(A)
+def solve(x1, y1, r1, x2, y2, r2):
+    r = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+    if r == 0 and r1 == r2:
+        print(-1)
+    elif r1 + r2 == r or r2 == r + r1 or r1 == r + r2:
+        print(1)
+    elif max(r1, r2, r) > r1+r2+r-max(r1, r2, r):
+        print(0)
+    else:
+        print(2)
+
+
+for i in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
+    solve(x1, y1, r1, x2, y2, r2)
