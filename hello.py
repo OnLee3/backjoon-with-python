59ebd2e29250b5ef4334e39af9075cde8f6cde74
LEON <dlwotjr2@gmail.com>
2021-11-05 16:43:41 +0900

    별 찍기 - 10

--- hello.py
+++ hello.py
@@ -1,16 +1,21 @@
 import sys
 
 N = int(sys.stdin.readline())
-A = [0, 1]
+
+stars = [[" " for _ in range(N)] for _ in range(N)]
 
 
-def solve(A, cnt):
-    if cnt > N:
-        print(A[N])
+def solve(size, x, y):
+    if size == 1:
+        stars[y][x] = "*"
     else:
-        A.append(A[cnt-2] + A[cnt-1])
-        cnt += 1
-        solve(A, cnt)
+        next_size = size // 3
+        for dy in range(3):
+            for dx in range(3):
+                if dy != 1 or dx != 1:
+                    solve(next_size, x + dx * next_size, y + dy * next_size)
 
 
-solve(A, 2)
+solve(N, 0, 0)
+for i in stars:
+    print("".join(i))
