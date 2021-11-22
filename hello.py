e98ab9fbe42dff4386c3ebb937a76487e419b1ab
LEON <dlwotjr2@gmail.com>
2021-11-22 14:59:47 +0900

    수정렬하기3 카운팅정렬

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 import sys
 
 N = int(sys.stdin.readline())
-List = [int(sys.stdin.readline()) for _ in range(N)]
-List.sort()
-[print(i) for i in List]
+countArray = [0 for _ in range(10001)]
+
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    countArray[num] += 1
+
+for i in range(1, 10001):
+    count = countArray[i]
+    for _ in range(count):
+        print(i)
