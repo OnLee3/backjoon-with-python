78b2d3669066f66dc69b294e6d09566a15fa55c3
LEON <dlwotjr2@gmail.com>
2021-11-22 16:39:53 +0900

    색종이 나누기

--- hello.py
+++ hello.py
@@ -1,13 +1,31 @@
 import sys
 
 N = int(sys.stdin.readline())
-countArray = [0 for _ in range(10001)]
+List = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+white = 0
+blue = 0
 
-for _ in range(N):
-    num = int(sys.stdin.readline())
-    countArray[num] += 1
 
-for i in range(1, 10001):
-    count = countArray[i]
-    for _ in range(count):
-        print(i)
+def divide(x, y, length):
+    first = List[y][x]
+
+    for i in range(length):
+        for j in range(length):
+            if List[y+i][x+j] != first:
+                divide(x, y, length//2)
+                divide(x + length//2, y, length//2)
+                divide(x, y + length//2, length//2)
+                divide(x + length//2, y + length//2, length//2)
+                return
+    if first == 0:
+        global white
+        white += 1
+    else:
+        global blue
+        blue += 1
+
+
+divide(0, 0, N)
+
+print(white)
+print(blue)
