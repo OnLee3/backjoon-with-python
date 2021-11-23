7bcc225728fb9fbb7f5d3b0d33f381e92b3d1471
LEON <dlwotjr2@gmail.com>
2021-11-23 20:56:09 +0900

    쿼드트리

--- hello.py
+++ hello.py
@@ -1,31 +1,26 @@
 import sys
 
 N = int(sys.stdin.readline())
-List = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-white = 0
-blue = 0
+List = [sys.stdin.readline().rstrip() for _ in range(N)]
+answer = ""
 
 
-def divide(x, y, length):
+def solve(x, y, length):
+    global answer
     first = List[y][x]
 
     for i in range(length):
         for j in range(length):
-            if List[y+i][x+j] != first:
-                divide(x, y, length//2)
-                divide(x + length//2, y, length//2)
-                divide(x, y + length//2, length//2)
-                divide(x + length//2, y + length//2, length//2)
+            if List[y + i][x + j] != first:
+                answer += "("
+                solve(x, y, length//2)
+                solve(x + length//2, y, length//2)
+                solve(x, y + length//2, length//2)
+                solve(x + length//2, y+length//2, length//2)
+                answer += ")"
                 return
-    if first == 0:
-        global white
-        white += 1
-    else:
-        global blue
-        blue += 1
+    answer += first
 
 
-divide(0, 0, N)
-
-print(white)
-print(blue)
+solve(0, 0, N)
+print(answer)
