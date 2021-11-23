8bb5da4afd09ca0189f5f7251b939348c3334a12
LEON <dlwotjr2@gmail.com>
2021-11-23 22:17:48 +0900

    곱셈

--- hello.py
+++ hello.py
@@ -1,33 +1,16 @@
 import sys
 
-N = int(sys.stdin.readline())
-List = [list(sys.stdin.readline().rstrip().split()) for _ in range(N)]
-a = 0
-b = 0
-c = 0
+a, b, c = map(int, sys.stdin.readline().split())
 
 
-def solve(x, y, length):
-    first = List[y][x]
-
-    for i in range(length):
-        for j in range(length):
-            if List[y + i][x + j] != first:
-                for k in range(3):
-                    for m in range(3):
-                        solve(x + (length//3*m), y + (length//3*k), length//3)
-                return
-
-    global a, b, c
-    if first == "-1":
-        a += 1
-    elif first == "0":
-        b += 1
+def solve(k, n):
+    if n == 0:
+        return 1
+    recur = solve(k, n//2) % c
+    if n % 2 == 0:
+        return recur * recur
     else:
-        c += 1
+        return recur * recur * k
 
 
-solve(0, 0, N)
-print(a)
-print(b)
-print(c)
+print(solve(a, b) % c)
