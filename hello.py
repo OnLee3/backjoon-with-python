de3314e826ff5f2d8bfe8ac8ece1cc70e73b7093
LEON <dlwotjr2@gmail.com>
2021-11-24 17:28:05 +0900

    이항계수 3

--- hello.py
+++ hello.py
@@ -1,16 +1,25 @@
 import sys
 
-a, b, c = map(int, sys.stdin.readline().split())
+N, K = map(int, sys.stdin.readline().split())
+p = 1000000007
 
 
-def solve(k, n):
-    if n == 0:
+def power(n, k):
+    if k == 0:
         return 1
-    recur = solve(k, n//2) % c
-    if n % 2 == 0:
-        return recur * recur
+    x = power(n, k//2)
+    if k % 2 == 0:
+        return (x ** 2) % p
     else:
-        return recur * recur * k
+        return (x ** 2 * n) % p
 
 
-print(solve(a, b) % c)
+Fact = [1 for _ in range(N+1)]
+
+for i in range(2, N+1):
+    Fact[i] = Fact[i-1] * i % p
+
+A = Fact[N]
+B = Fact[K] * Fact[N-K] % p
+
+print((A % p) * (power(B, p-2) % p) % p)
