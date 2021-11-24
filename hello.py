617e1feaa1178e1ebe87055e20eb7684b72b3f9c
LEON <dlwotjr2@gmail.com>
2021-11-24 22:41:00 +0900

    책페이지

--- hello.py
+++ hello.py
@@ -1,25 +1,24 @@
 import sys
 
-N, K = map(int, sys.stdin.readline().split())
-p = 1000000007
+N = int(sys.stdin.readline())
+count = [0 for _ in range(10)]
 
 
-def power(n, k):
-    if k == 0:
-        return 1
-    x = power(n, k//2)
-    if k % 2 == 0:
-        return (x ** 2) % p
-    else:
-        return (x ** 2 * n) % p
+def pageCount(pages, pValue):
+    if pages:
+        replaced = int(str(pages//10) + "9")
+        gap = replaced - pages
+
+        for i in range(len(count)):
+            count[i] += (pages//10 + 1) * pValue
+        for i in range(10-gap, 10):
+            count[i] -= pValue
+        for i in list(str(pages)[:-1]):
+            count[int(i)] -= gap * pValue
+
+        count[0] -= pValue
+        pageCount(pages//10, pValue*10)
 
 
-Fact = [1 for _ in range(N+1)]
-
-for i in range(2, N+1):
-    Fact[i] = Fact[i-1] * i % p
-
-A = Fact[N]
-B = Fact[K] * Fact[N-K] % p
-
-print((A % p) * (power(B, p-2) % p) % p)
+pageCount(N, 1)
+print(" ".join(map(str, count)))
