5e8a3dc72d5dfd0642931687fbc3ea6009e8e651
LEON <dlwotjr2@gmail.com>
2021-11-26 17:26:42 +0900

    소트인사이드ㅡ

--- hello.py
+++ hello.py
@@ -1,31 +1,5 @@
 import sys
-from collections import Counter
 
-N = int(sys.stdin.readline())
-A = sorted([int(sys.stdin.readline()) for _ in range(N)])
-
-
-def Avg():
-    return round(sum(A) / N)
-
-
-def Mid():
-    return A[N // 2]
-
-
-def Mode():
-    mode = Counter(A).most_common(2)
-    if len(mode) > 1 and mode[0][1] == mode[1][1]:
-        return mode[1][0]
-    else:
-        return mode[0][0]
-
-
-def Range():
-    return max(A) - min(A)
-
-
-print(Avg())
-print(Mid())
-print(Mode())
-print(Range())
+N = list(str(sys.stdin.readline().rstrip()))
+N.sort(reverse=True)
+print("".join(N))
