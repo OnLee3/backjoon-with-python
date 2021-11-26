95673efa0e0ae8d7d3f0ac4c832b286f44d310c6
LEON <dlwotjr2@gmail.com>
2021-11-26 18:27:05 +0900

    좌표 정렬하기 2

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 import sys
 
-N = list(str(sys.stdin.readline().rstrip()))
-N.sort(reverse=True)
-print("".join(N))
+N = int(sys.stdin.readline().rstrip())
+List = []
+
+for i in range(N):
+    a, b = map(int, sys.stdin.readline().split())
+    List.append([b, a])
+
+List = sorted(List)
+
+for i in range(N):
+    print(List[i][1], List[i][0])
