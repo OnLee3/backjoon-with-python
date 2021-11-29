2d67e2451e54082bd9149b3d793833718d5d9d64
LEON <dlwotjr2@gmail.com>
2021-11-29 15:00:58 +0900

    N과과M(4)

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 import sys
-from itertools import combinations
+from itertools import combinations_with_replacement
 
 N, M = map(int, sys.stdin.readline().split())
 List = [i for i in range(1, N+1)]
 
-for i in list(combinations(List, M)):
+for i in list(combinations_with_replacement(List, M)):
     print(" ".join(map(str, i)))
