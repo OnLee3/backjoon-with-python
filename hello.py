f122afc019e311269f0107b1a34c35a49ad0f7ad
LEON <dlwotjr2@gmail.com>
2021-11-30 19:53:28 +0900

    회의실 배정

--- hello.py
+++ hello.py
@@ -1,16 +1,23 @@
 import sys
+from operator import itemgetter
 
 
 def input():
     return sys.stdin.readline().rstrip()
 
 
-N, K = map(int, input().split())
-List = [int(input()) for _ in range(N)]
-List.reverse()
-
+N = int(input())
+Schedule = []
+et = 0
 cnt = 0
-for i in List:
-    cnt += K // i
-    K = K % i
+
+for _ in range(N):
+    start, end = map(int, input().split())
+    Schedule.append([start, end])
+Schedule = sorted(Schedule, key=itemgetter(1, 0))
+
+for meeting in Schedule:
+    if meeting[0] >= et:
+        cnt += 1
+        et = meeting[1]
 print(cnt)
