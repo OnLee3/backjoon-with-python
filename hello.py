a278982b39e6537167a66b80c493155d24413cd4
LEON <dlwotjr2@gmail.com>
2021-12-03 17:18:38 +0900

    01타일

--- hello.py
+++ hello.py
@@ -5,28 +5,12 @@ def input():
     return sys.stdin.readline().rstrip()
 
 
-MAX = 21
-dp = [[[0] * MAX for __ in range(MAX)]for _ in range(MAX)]
+def solve(N):
+    dp = [0, 1, 2, 3, 5, 8]
+    if N > 5:
+        for i in range(N-5):
+            dp.append((dp[-2] + dp[-1]) % 15746)
+    return dp[N]
 
 
-def solve(a, b, c):
-    if a <= 0 or b <= 0 or c <= 0:
-        return 1
-    elif a > 20 or b > 20 or c > 20:
-        return solve(20, 20, 20)
-
-    if dp[a][b][c]:
-        return dp[a][b][c]
-    if a < b < c:
-        dp[a][b][c] = solve(a, b, c-1) + solve(a, b-1, c-1) - solve(a, b-1, c)
-    else:
-        dp[a][b][c] = solve(a-1, b, c) + solve(a-1, b-1, c) + \
-            solve(a-1, b, c-1) - solve(a-1, b-1, c-1)
-    return dp[a][b][c]
-
-
-while True:
-    a, b, c = map(int, input().split())
-    if a == -1 and b == -1 and c == -1:
-        break
-    print(f"w({a}, {b}, {c}) = {solve(a, b, c)}")
+print(solve(int(input())))
