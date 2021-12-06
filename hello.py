ec3f066b607eff2b6d76a1dca153a495a1e015dc
LEON <dlwotjr2@gmail.com>
2021-12-06 17:01:55 +0900

    가운데를 말해요

--- hello.py
+++ hello.py
@@ -6,17 +6,21 @@ def input():
     return sys.stdin.readline().rstrip()
 
 
-heap = []
+leftHeap = []
+rightHeap = []
 
 
 def solve(x):
-    if x == 0:
-        if len(heap) == 0:
-            print(0)
-        else:
-            print(heapq.heappop(heap)[1])
+    if len(leftHeap) == len(rightHeap):
+        heapq.heappush(leftHeap, (-x, x))
     else:
-        heapq.heappush(heap, (abs(x), x))
+        heapq.heappush(rightHeap, x)
+    if rightHeap and leftHeap[0][1] > rightHeap[0]:
+        Min = heapq.heappop(rightHeap)
+        Max = heapq.heappop(leftHeap)[1]
+        heapq.heappush(leftHeap, (-Min, Min))
+        heapq.heappush(rightHeap, Max)
+    print(leftHeap[0][1])
 
 
 [solve(int(input())) for _ in range(int(input()))]
