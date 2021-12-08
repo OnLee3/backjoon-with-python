54bc1ae6ebb68523886105dd6b7f56efc680398e
LEON <dlwotjr2@gmail.com>
2021-12-08 18:52:31 +0900

    카드 정렬하기

--- solve.py
+++ solve.py
@@ -1,20 +1,29 @@
 import sys
+import heapq
 
 
 def input():
     return sys.stdin.readline().rstrip()
 
 
-def solve(H):
-    dp = [1, 1, 3, 5]
-    if H > 3:
-        for _ in range(H-3):
-            current = 0
-            for i in range(len(dp)-1):
-                current += dp[i]
-            dp.append(current*2+1)
-    return dp[H]
+def solve(N):
+    if N == 1:
+        return 0
+    else:
+        heap = []
+        answer = 0
+
+        for _ in range(N):
+            heapq.heappush(heap, int(input()))
+
+        for _ in range(N-1):
+            deck1 = heapq.heappop(heap)
+            deck2 = heapq.heappop(heap)
+            answer += deck1 + deck2
+            heapq.heappush(heap, deck1 + deck2)
+
+        return answer
 
 
-H = int(input())
-print(solve(H))
+N = int(input())
+print(solve(N))
