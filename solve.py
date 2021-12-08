2c47f999a92f0b2c130b334cbb6c67ccef8d4c1b
LEON <dlwotjr2@gmail.com>
2021-12-08 17:41:50 +0900

    완전 이진 트리 도로 네트워크

--- solve.py
+++ solve.py
@@ -5,46 +5,16 @@ def input():
     return sys.stdin.readline().rstrip()
 
 
-class Node:
-    def __init__(self, root, left, right):
-        self.root = root
-        self.left = left
-        self.right = right
+def solve(H):
+    dp = [1, 1, 3, 5]
+    if H > 3:
+        for _ in range(H-3):
+            current = 0
+            for i in range(len(dp)-1):
+                current += dp[i]
+            dp.append(current*2+1)
+    return dp[H]
 
 
-def preorder(node):
-    print(node.root, end="")
-    if node.left != ".":
-        preorder(tree[node.left])
-    if node.right != ".":
-        preorder(tree[node.right])
-
-
-def inorder(node):
-    if node.left != ".":
-        inorder(tree[node.left])
-    print(node.root, end="")
-    if node.right != ".":
-        inorder(tree[node.right])
-
-
-def postorder(node):
-    if node.left != ".":
-        postorder(tree[node.left])
-    if node.right != ".":
-        postorder(tree[node.right])
-    print(node.root, end="")
-
-
-N = int(input())
-tree = {}
-
-for _ in range(N):
-    root, left, right = map(str, input().split())
-    tree[root] = Node(root=root, left=left, right=right)
-
-preorder(tree['A'])
-print()
-inorder(tree['A'])
-print()
-postorder(tree['A'])
+H = int(input())
+print(solve(H))
