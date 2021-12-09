38f8cf5412d4e72bec18a9237802d006a7ae2c90
LEON <dlwotjr2@gmail.com>
2021-12-09 19:20:35 +0900

    N-Queen

--- solve.py
+++ solve.py
@@ -1,21 +1,31 @@
 import sys
-import heapq
 
 
 def input():
     return sys.stdin.readline().rstrip()
 
 
-N, K = map(int, input().split())
-Prime = list(map(int, input().split()))
-heap = []
-[heapq.heappush(heap, x) for x in Prime]
+def check(x):
+    for i in range(x):
+        if col[x] == col[i] or abs(col[i]-col[x]) == x-i:
+            return False
+    return True
 
-for i in range(K):
-    current = heapq.heappop(heap)
-    for j in range(N):
-        heapq.heappush(heap, current * Prime[j])
-        if current % Prime[j] == 0:
-            break
-else:
-    print(current)
+
+def dfs(x):
+    global answer
+    if x == N:
+        answer += 1
+        return
+    for i in range(N):
+        col[x] = i
+        if check(x):
+            dfs(x+1)
+
+
+N = int(input())
+answer = 0
+col = [0]*15
+
+dfs(0)
+print(answer)
