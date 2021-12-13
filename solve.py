648b1b1316ff509572024d74b9cd27cb1348c478
LEON <dlwotjr2@gmail.com>
2021-12-13 19:12:23 +0900

    균형잡힌 세상

--- solve.py
+++ solve.py
@@ -5,23 +5,33 @@ def input():
     return sys.stdin.readline().rstrip()
 
 
-N = int(input())
-
-
-def solve(PS):
+def solve(S):
     Stack = []
-    for x in PS:
-        if x == "(":
+    for x in S:
+        if x == "(" or x == "[":
             Stack.append(x)
-        else:
-            if len(Stack):
-                Stack.pop()
+        elif x == ")":
+            if Stack:
+                last = Stack.pop()
+                if last != "(":
+                    return "no"
             else:
-                return "NO"
-    if len(Stack):
-        return "NO"
+                return "no"
+        elif x == "]":
+            if Stack:
+                last = Stack.pop()
+                if last != "[":
+                    return "no"
+            else:
+                return "no"
+    if Stack:
+        return "no"
     else:
-        return "YES"
+        return "yes"
 
 
-[print(solve(input())) for _ in range(N)]
+while True:
+    S = input()
+    if S == ".":
+        break
+    print(solve(S))
