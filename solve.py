b04d4e165f2e294454798d2f32564f32e813733d
LEON <dlwotjr2@gmail.com>
2021-12-28 19:49:26 +0900

    DFS와 BFS

--- solve.py
+++ solve.py
@@ -1,37 +1,45 @@
 import sys
+from collections import deque
 
 
 def input():
     return sys.stdin.readline().rstrip()
 
 
-def solve(S):
-    Stack = []
-    for x in S:
-        if x == "(" or x == "[":
-            Stack.append(x)
-        elif x == ")":
-            if Stack:
-                last = Stack.pop()
-                if last != "(":
-                    return "no"
-            else:
-                return "no"
-        elif x == "]":
-            if Stack:
-                last = Stack.pop()
-                if last != "[":
-                    return "no"
-            else:
-                return "no"
-    if Stack:
-        return "no"
-    else:
-        return "yes"
+N, M, V = map(int, input().split())
+visited = [False] * (N+1)
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, N+1):
+    graph[i].sort()
 
 
-while True:
-    S = input()
-    if S == ".":
-        break
-    print(solve(S))
+def dfs(v):
+    visited[v] = True
+    print(v, end=" ")
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+
+
+def bfs(v):
+    visited[v] = True
+    Queue = deque([v])
+    while Queue:
+        v = Queue.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
+                Queue.append(i)
+                visited[i] = True
+
+
+dfs(V)
+print()
+visited = [False] * (N+1)
+bfs(V)
