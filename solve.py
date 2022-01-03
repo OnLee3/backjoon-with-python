723017be9437cedd5eb6309fda02c138bb2d4856
LEON <dlwotjr2@gmail.com>
2022-01-03 20:48:30 +0900

    유기농 배추

--- solve.py
+++ solve.py
@@ -1,40 +1,41 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10 ** 6)
 
 
 def input():
     return sys.stdin.readline().rstrip()
 
 
-N, M = map(int, input().split())
-graph = [list(map(int, input().split()))for _ in range(M)]
-queue = deque([])
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-answer = 0
-
-for i in range(M):
-    for j in range(N):
-        if graph[i][j] == 1:
-            queue.append([i, j])
-
-
-def bfs():
-    while queue:
-        x, y = queue.popleft()
-        for i in range(len(dx)):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < M and 0 <= ny < N and graph[nx][ny] == 0:
-                queue.append([nx, ny])
-                graph[nx][ny] = graph[x][y] + 1
+def solve():
+    cnt = 0
+    for i in range(K):
+        x, y = map(int, input().split())
+        graph[y][x] = 1
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 1:
+                dfs(j, i)
+                cnt += 1
+    print(cnt)
 
 
-bfs()
-for i in graph:
-    for j in i:
-        if j == 0:
-            print(-1)
-            exit(0)
-    answer = max(answer, max(i))
-print(answer-1)
+def dfs(x, y):
+    if graph[y][x] == 1:
+        graph[y][x] = 0
+        if x-1 >= 0:
+            dfs(x-1, y)
+        if x+1 < M:
+            dfs(x+1, y)
+        if y-1 >= 0:
+            dfs(x, y-1)
+        if y+1 < N:
+            dfs(x, y+1)
+    else:
+        return
+
+
+T = int(input())
+for _ in range(T):
+    M, N, K = map(int, input().split())
+    graph = [[0] * M for _ in range(N)]
+    solve()
