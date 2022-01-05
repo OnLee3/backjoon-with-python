418e97e66c25680078bb637462264f7467fb8235
LEON <dlwotjr2@gmail.com>
2022-01-05 21:14:18 +0900

    소가 길을 건너간 이유 4

--- solve.py
+++ solve.py
@@ -1,29 +1,24 @@
 import sys
-from itertools import combinations
 
 
 def input():
     return sys.stdin.readline().rstrip()
 
 
-def isValid(perm):
-    vowelCount = 0
-    consonantCount = 0
-    for letter in perm:
-        if letter in vowel:
-            vowelCount += 1
-        else:
-            consonantCount += 1
-    if consonantCount < 2 or vowelCount < 1:
-        return False
-    return True
+C, N = map(int, input().split())
+chickens = []
+cows = []
+answer = 0
 
+[chickens.append(int(input())) for _ in range(C)]
+[cows.append(list(map(int, input().split()))) for _ in range(N)]
+chickens.sort()
+cows.sort(key=lambda x: (x[1], x[0]))
 
-vowel = ['a', 'e', 'i', 'o', 'u']
-
-L, C = map(int, input().split())
-List = sorted(list(input().split()))
-perm = list(combinations(List, L))
-result = list(filter(isValid, perm))
-
-[print("".join(i)) for i in result]
+for cow in cows:
+    for chicken in chickens:
+        if cow[0] <= chicken <= cow[1]:
+            answer += 1
+            chickens.remove(chicken)
+            break
+print(answer)
