37e962322807193326dda7887604ac46181adc8e
이재석 <83897458+OnLee3@users.noreply.github.com>
2022-06-27 18:08:00 +0900

    [Silver I] Title: 1로 만들기 2, Time: 952 ms, Memory: 445336 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,18 +1,13 @@
-import sys
-
-def input():
-  return sys.stdin.readline().rstrip()  
-
 n = int(input())
-dp = [[] for _ in range(n+1)]
-dp[1].append(1)
+dp = [[], [1]]
 
 for i in range(2, n+1):
-  dp[i] = dp[i-1]+[i]
+  dp.append(dp[i-1]+[i])
   if i%3 == 0 and len(dp[i]) > len(dp[i//3])+1:
     dp[i] = dp[i//3] + [i]
   if i%2 == 0 and len(dp[i]) > len(dp[i//2])+1:
     dp[i] = dp[i//2] + [i]
+    
 answer = list(map(str, reversed(dp[n])))
 print(len(answer) - 1)
 print(" ".join(answer))
