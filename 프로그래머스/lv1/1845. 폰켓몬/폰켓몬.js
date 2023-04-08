fbc04741c22f7f9352c3985b49dc27ebe664f973
이재석 <83897458+OnLee3@users.noreply.github.com>
2023-04-08 16:45:41 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,4 @@
 function solution(nums) {
-    // 집합 set 생성
-    const pokemons = new Set();
-    // nums for문 돌면서 set에 setting
-    for (const num of nums) {
-        pokemons.add(num);
-        if (pokemons.size >= nums.length/2) break;
-    }
-    // set 길이 반환
-    return pokemons.size;
+    const uniquePokemons = new Set(nums);
+    return Math.min(uniquePokemons.size, nums.length / 2);
 }
