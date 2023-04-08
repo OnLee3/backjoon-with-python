8cce0b60b5e542f170a0766025001097203abc7b
이재석 <83897458+OnLee3@users.noreply.github.com>
2023-04-08 17:05:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 96.33 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,27 +1,9 @@
 function solution(participant, completion) {
-    const participants = {};
-    const completions = {};
-    
+    participant.sort();
+    completion.sort();
+
     for (let i = 0; i < participant.length; i++) {
-        if (participants.hasOwnProperty(participant[i])) {
-            participants[participant[i]]++;
-        } else {
-            participants[participant[i]] = 1;
-        }
-        if (i !== participant.length - 1) {
-            if (completions.hasOwnProperty(completion[i])) {
-                completions[completion[i]]++;
-            } else {
-                completions[completion[i]] = 1;
-            }
-        }
-    }
-    
-    for (let i = 0; i < completion.length; i++) {
-        participants[completion[i]] -= 1;
-    }
-    for (let i = 0; i < participant.length; i++) {
-        if (participants[participant[i]]) {
+        if (participant[i] !== completion[i]) {
             return participant[i];
         }
     }
