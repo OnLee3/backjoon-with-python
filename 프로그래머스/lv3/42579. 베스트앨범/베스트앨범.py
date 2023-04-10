3e4619da126d73452d443444630a2606a6fa223e
이재석 <83897458+OnLee3@users.noreply.github.com>
2023-04-10 22:00:32 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,17 +1,21 @@
 def solution(genres, plays):
     # playsMap = new Map({genre : { total_plays: number, songs: [(idx, plays)...]}, ...}...{...})
-    genresMap = dict()
+    genre_plays = dict()
     for i in range(len(genres)):
-        if genres[i] not in genresMap:
-            genresMap[genres[i]] = { 'total_plays': 0, 'songs': []}
-        genresMap[genres[i]]['total_plays'] += plays[i]
-        genresMap[genres[i]]['songs'].append((i, plays[i]))
+        genre = genres[i]
+        play = plays[i]
+        if genres[i] not in genre_plays:
+            genre_plays[genre] = { 'total_plays': 0, 'songs': []}
+        genre_plays[genre]['total_plays'] += play
+        genre_plays[genre]['songs'].append((i, play))
+        
     # playsMap.sort by sum of plays in genre
-    sortedMap = sorted(genresMap.keys(), key = lambda genre: genresMap[genre]['total_plays'], reverse = True)
+    sorted_genres = sorted(genre_plays.keys(), key = lambda genre: genre_plays[genre]['total_plays'], reverse = True)
+    
     # Get Value from front.
     answer = []
-    for genre in sortedMap:
-        sorted_songs = sorted(genresMap[genre]['songs'], key = lambda song: (-song[1], song[0]))
+    for genre in sorted_genres:
+        sorted_songs = sorted(genre_plays[genre]['songs'], key = lambda song: (-song[1], song[0]))
         for i in range(min(2, len(sorted_songs))):
             answer.append(sorted_songs[i][0])
     return answer
